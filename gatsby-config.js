1b5753e910ee2ee04a978e0000d4c24ca82a11a8
Y0o0Y1 <75071207+Y0o0Y1@users.noreply.github.com>
2020-11-26 14:01:59 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
     {
       resolve: `gatsby-source-datocms`,
       options: {
-        apiToken: process.env.DATO_API_TOKEN,
+        apiToken: ed5760acae2642a8dc82386c3d3ec0,
       },
     },
   ],
